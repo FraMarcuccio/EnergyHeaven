a42d3afe06a67de7fee1316773c61e6c9b87fbd2
FraMarcuccio <francesco.marcuccio.fm@gmail.com>
2023-01-05 01:21:14 +0100

    created CSS structure + modified HTML structure

--- Dapp-EnergyHeaven/contracts/EnergyHeaven.sol
+++ Dapp-EnergyHeaven/contracts/EnergyHeaven.sol
@@ -4,6 +4,10 @@ pragma solidity >=0.4.0 <0.9.0;
 contract EnergyHeaven{
     address payable public minter;
 
+    uint public constant OPERATIONS_FOR_REWARDING = 50;
+    uint private operation_counter;
+    mapping(address => uint) private selling_history;
+
     address[] producers;
     mapping(address => uint) public userBalance; // balance dei token di ogni utente
 
@@ -22,9 +26,13 @@ contract EnergyHeaven{
 
     event AcquiringTokens(address user, uint tokens);
     event TokensToEth(address user, uint tokens);
+    event JoinedAsProducer(address user);
+    event EnergyBought(address buyer, uint bought_amount);
+    event piggyBankBroken();
 
     constructor(){
         minter = payable(msg.sender);
+        operation_counter = 0;
     }
 
     function join_as_producer() public{
@@ -41,7 +49,35 @@ contract EnergyHeaven{
         userBalance[msg.sender] -= JOIN_AMOUNT;
         piggyBank += JOIN_AMOUNT;
         producers.push(msg.sender);
+        emit JoinedAsProducer(msg.sender);
+        selling_history[msg.sender] = 0;
+
+        if(operation_counter == OPERATIONS_FOR_REWARDING)
+            breaking_piggyBank();
     }
+
+    function breaking_piggyBank() private{
+        uint part = 0;
+        uint total_history = 0;
+        for(uint i=0; i<producers.length; i++){
+            total_history += selling_history[producers[i]];
+        }
+        part = piggyBank / total_history;
+        for(uint i=0; i<producers.length; i++){
+            uint reward = part * selling_history[producers[i]];
+            userBalance[producers[i]] += reward;
+            piggyBank -= reward;
+            selling_history[producers[i]] = 0;
+        }
+        operation_counter = 0;
+        emit piggyBankBroken();
+    }
+
+    function add_piggyBank(uint tokens) public{
+        require(userBalance[msg.sender] >= tokens, "Not enough tokens");
+        userBalance[msg.sender] -= tokens;
+        piggyBank += tokens;
+    }   
 
     function obtain_tokens() public payable{
         require(msg.value >= PRICE, "Not enough value for a token");
@@ -74,6 +110,8 @@ contract EnergyHeaven{
         
         energyList[msg.sender].amount += amount;
         energyList[msg.sender].price = price;
+        selling_history[msg.sender] += amount;
+        operation_counter += 1;
     }
 
     // Funzione helper per verificare se un indirizzo è presente nell'array dei produttori
@@ -93,22 +131,33 @@ contract EnergyHeaven{
         address[] memory cheapest;
         uint price; 
         (cheapest,price) = find_cheapest();
-        
+        require(cheapest.length>1, "Energy not available");
         require(tokens>price, "Not enough tokens");
+
         bought_amount = 0;
         uint i = 0;
-        while(tokens > price){
-            energyList[cheapest[i]].amount -= 1;
-            bought_amount += 1;
-            userBalance[cheapest[i]] += price;
-            userBalance[msg.sender] -= price;
-            tokens -= price;
-            i += 1;
-            if(i>=cheapest.length) i=0;
+        while(tokens >= price){
+            if(energyList[cheapest[i]].amount>0){
+                energyList[cheapest[i]].amount -= 1;
+                bought_amount += 1;
+                userBalance[cheapest[i]] += price;
+                userBalance[msg.sender] -= price;
+                tokens -= price;
+            }
+            i +=1;
+            if(end_cheapest_energy(cheapest)) break;
+            if(i>cheapest.length-1) i=0;
         }  
         
+        emit EnergyBought(msg.sender, bought_amount);
         return (true, bought_amount);
     }
+
+    function end_cheapest_energy(address[] memory cheapest) private view returns(bool){
+        for(uint i=0; i<cheapest.length; i++)
+            if(energyList[cheapest[i]].amount>0) return false;
+        return true;
+    }   
 
     function find_cheapest() private view returns(address[] memory cheapest, uint price) {
         //ritorna tutti i producers che stanno vendendo ad un minor prezzo in un array e il prezzo
@@ -117,7 +166,7 @@ contract EnergyHeaven{
         //inizializzo price con un valore più alto di quello possibile
         //uint price = 2**256 -1; 
 
-        price = uint(1); //da definire ancora in fase di test se funziona correttamente
+        price = (2**256)-1; //da definire ancora in fase di test se funziona correttamente
 
         uint j = 0; //indice per l'array cheapest
         for(uint i = 0; i < producers.length; i++){
@@ -126,7 +175,7 @@ contract EnergyHeaven{
             uint p = energyList[producers[i]].price;
     
             //se è il più basso che ho visto fino ad ora, lo mantengo e resetto l'array cheapest
-            if(p < price){
+            if(p < price && energyList[producers[i]].amount > 0){
                 cheapest = new address[](producers.length);
                 j = 0;
                 price = p;
